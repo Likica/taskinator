97bf4568d77e12bc9edd42283d1904b75cb3509c
Coco Puff <fahrija_wyroski@live.com>
2021-03-18 09:13:19 -0500

    added event.preventdefault to taskhandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); // document.querySelector() - exchanged console.log for expressions to create, style new task item, add the text and append this element to the tasklist
 
 //Add createTaskHandler function to dynamically create the task item Needs to be added BEFORE ebentlistener!
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+
 };
 
 //Eventlistener 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
